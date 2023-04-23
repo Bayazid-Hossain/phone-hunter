49f9b24fdb752dabe7f06e833345dcfbebfa49b0
Bayazid-Hossain <bayazidh570@gmail.com>
2023-04-23 08:23:28 +0600

    phone hunter

---
+++ js/error.js
@@ -0,0 +1,46 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
+// .then(data => console.log(data));
+//  (error => console.log(error))
+
+// try{
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users'),
+// const data = await res.json()
+
+// }
+// catch{
+
+// }
+
+
+// // document.getElementById('abc')
+// // synchronous asynchronous
+// console.log(1)
+// setTimeout(() =>{
+//     console.log(2);
+// }, 3000)
+// console.log(3)
+
+
+const dreamGirl = [
+    {
+     sokina: {
+      name: "bbu",
+      height: "5.4",
+      family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+      age: undefined,
+      contactInfo: [
+       {
+        facebook: {
+         link: "https://www.facebook.com/",
+         followers: "12545",
+         status: "single",
+         friendsList: [{ name: "rofik" }, undefined],
+        },
+       },
+       { instagram: "https://www.instagram.com/" },
+      ],
+     },
+    },
+   ];
+   
